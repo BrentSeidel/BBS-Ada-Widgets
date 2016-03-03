5ef9c340721df1b147d3ec0c5be0b9bf92bcddf4
BrentSeidel <brent@Minerva.local>
2016-03-03 08:18:05 -0700

    Begin work on Attitude Indicator
    Start working on an attitude indicator widget.  This currently just
    displays pitch and roll.

---
+++ bbs-widget-ai.adb
@@ -0,0 +1,277 @@
+package body bbs.widget.ai is
+
+   function Get_Type return Glib.GType is
+   begin
+       Glib.Object.Initialize_Class_Record
+         (Ancestor     => Gtk.Drawing_Area.Get_Type,
+          Class_Record => Klass,
+          Type_Name    => "bbs_ai",
+          Signals      => Glib.Object.No_Signals,
+          Parameters   => Glib.Object.Null_Parameter_Types);
+      return Klass.The_Type;
+   end;
+
+   procedure gtk_new(self : in out bbs_ai) is
+   begin
+      self := new bbs_ai_record;
+      initialize(self);
+   end;
+
+   procedure initialize(self : not null access bbs_ai_record'class) is
+   begin
+      Glib.Object.G_New(Object => self,
+                        Typ    => Get_Type);
+      Gtk.Drawing_Area.Initialize(self);
+      self.On_Draw(draw_ai'access, True);
+   end;
+   --
+   -- Attach the window and request the size as well as setting reasonable
+   -- defaults for most values.
+   --
+   procedure setup(self : not null access bbs_ai_record'class;
+                  radius : Float; parent : Gdk.Gdk_Window) is
+   begin
+      self.radius := radius;
+      self.size := Integer(self.radius*2.0 + 50.0);
+      self.slew := false;
+      self.failed := False;
+      self.pitch := 0.0;
+      self.roll := 0.0;
+      self.slew_rate := 0.0;
+      self.dsp_pitch := 0.0;
+      self.dsp_roll := 0.0;
+      Glib.Properties.Set_Property(self, Gtk.Widget.Height_Request_Property, Glib.Gint(self.size));
+      Glib.Properties.Set_Property(self, Gtk.Widget.Width_Request_Property, Glib.Gint(self.size));
+      self.Set_Window(parent);
+      self.Set_Has_Window(True);
+   end;
+
+   procedure set_value(self : in out bbs_ai_record'Class; pitch : Float; roll : Float) is
+   begin
+      self.pitch := pitch;
+      self.roll := roll;
+      if (not self.slew) then
+         self.dsp_pitch := self.pitch;
+         self.dsp_roll := self.roll;
+         self.Queue_Draw;
+      end if;
+   end;
+
+   procedure set_failed(self : in out bbs_ai_record'Class; value : Boolean) is
+   begin
+      self.failed := value;
+      self.Queue_Draw;
+   end;
+
+   procedure set_slew(self : in out bbs_ai_record'Class; value : Boolean; rate : Float) is
+   begin
+      self.slew_rate := rate;
+      self.slew := value;
+      if (value) then
+         self.callback_id := self.Add_Tick_Callback(slew_handler'Access, null);
+      else
+         self.Remove_Tick_Callback(self.callback_id);
+         self.dsp_pitch := self.pitch;
+         self.dsp_roll := self.roll;
+      end if;
+   end;
+
+   function slew_handler(Self : not null access Gtk.Widget.Gtk_Widget_Record'Class;
+                         Frame_Clock : not null access Gdk.Frame_Clock.Gdk_Frame_Clock_Record'Class) return Boolean is
+      me : constant bbs_ai := bbs_ai(self);
+   begin
+      if (me.dsp_pitch /= me.pitch) or (me.dsp_roll /= me.roll) then
+         if (me.dsp_pitch < me.pitch) then
+            if ((me.dsp_pitch - me.pitch) < me.slew_rate) then
+               me.dsp_pitch := me.pitch;
+            else
+               me.dsp_pitch := me.dsp_pitch + me.slew_rate;
+            end if;
+         else
+            if ((me.dsp_pitch - me.pitch) < me.slew_rate) then
+               me.dsp_pitch := me.pitch;
+            else
+               me.dsp_pitch := me.dsp_pitch - me.slew_rate;
+            end if;
+         end if;
+         if (me.dsp_roll < me.roll) then
+            if ((me.dsp_roll - me.roll) < me.slew_rate) then
+               me.dsp_roll := me.roll;
+            else
+               me.dsp_roll := me.dsp_roll + me.slew_rate;
+            end if;
+         else
+            if ((me.dsp_roll - me.roll) < me.slew_rate) then
+               me.dsp_roll := me.roll;
+            else
+               me.dsp_roll := me.dsp_roll - me.slew_rate;
+            end if;
+         end if;
+         self.Queue_Draw;
+      end if;
+      return True;
+   end;
+
+   function draw_ai(Self : access Gtk.Widget.Gtk_Widget_Record'Class; context : Cairo.Cairo_Context) return boolean is
+      me : constant bbs_ai := bbs_ai(self);
+      matrix : aliased Cairo.Cairo_Matrix;
+   begin
+      if (me.failed) then
+         draw_failed(me, context);
+      else
+         Cairo.Translate(context, Glib.Gdouble(Float(me.size)/2.0),
+                         Glib.Gdouble(Float(me.size)/2.0));
+         Cairo.Get_Matrix(context, matrix'Access);
+         me.draw_horizon(context);
+         Cairo.Set_Matrix(context, matrix'Access);
+         draw_fixed(me, context);
+         Cairo.Set_Matrix(context, matrix'Access);
+         draw_roll(me, context);
+      end if;
+      return True;
+   end;
+   --
+   -- Draw the moving horizon.
+   --
+   procedure draw_horizon(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context) is
+   begin
+      --
+      -- Translate and rotate for pitch and roll
+      --
+      Cairo.Rotate(context, Glib.Gdouble(self.roll*Ada.Numerics.Pi/180.0));
+      Cairo.Translate(context, 0.0, Glib.Gdouble(self.pitch));
+      --
+      -- Draw the ground
+      --
+      Cairo.Set_Source_Rgb(context, 0.7, 0.6, 0.3);
+      Cairo.Move_To(context, 0.0, 0.0);
+      Cairo.Arc(context, 0.0, 0.0, Glib.Gdouble(self.radius*4.0), 0.0, Glib.Gdouble(Ada.Numerics.Pi));
+      Cairo.Fill(context);
+      --
+      -- Draw the sky
+      --
+      Cairo.Set_Source_Rgb(context, 0.5, 0.5, 0.9);
+      Cairo.Move_To(context, 0.0, 0.0);
+      Cairo.Arc(context, 0.0, 0.0, Glib.Gdouble(self.radius*4.0), Glib.Gdouble(Ada.Numerics.Pi), Glib.Gdouble(two_pi));
+      Cairo.Fill(context);
+      --
+      -- Draw a horizon line
+      --
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 0.0, 0.0, 0.0);
+      Cairo.Move_To(context, Glib.Gdouble(-Float(self.size)), 0.0);
+      Cairo.Line_To(context, Glib.Gdouble(Float(self.size)), 0.0);
+      Cairo.Stroke(context);
+      --
+      -- Draw pitch markers
+      --
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 0.0, 0.0, 0.0);
+      for x in -3 .. 3 loop
+         Cairo.Move_To(context, -10.0, Glib.Gdouble(Float(x*10)));
+         Cairo.Line_To(context, 10.0, Glib.Gdouble(Float(x*10)));
+      end loop;
+      Cairo.Stroke(context);
+   end;
+   --
+   -- Draw the fixed parts of the attitude indicator.
+   --
+   procedure draw_fixed(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context) is
+      matrix : aliased Cairo.Cairo_Matrix;
+   begin
+      --
+      -- Draw roll arc
+      --
+      Cairo.Set_Line_Width(context, 2.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 1.0, 1.0);
+      Cairo.Move_To(context, Glib.Gdouble(-(self.radius)/sqrt_2), Glib.Gdouble(-(self.radius)/sqrt_2));
+      Cairo.Arc(context, 0.0, 0.0, Glib.Gdouble(self.radius), Glib.Gdouble(5.0*Ada.Numerics.Pi/4.0),
+                Glib.Gdouble(7.0*Ada.Numerics.Pi/4.0));
+      Cairo.Stroke(context);
+      --
+      -- Add tick marks at +/-45, +/-30, +/-15, and 0 degrees roll
+      --
+      Cairo.Get_Matrix(context, matrix'Access);
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 1.0, 1.0);
+      Cairo.Rotate(context, Glib.Gdouble((-45 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((-30 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((-15 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((15 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((30 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Rotate(context, Glib.Gdouble((45 + 180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Stroke(context);
+      --
+      -- Draw zero degree triangle
+      --
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 1.0, 1.0);
+      Cairo.Rotate(context, Glib.Gdouble((180)*Ada.Numerics.Pi/180.0));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Line_To(context, -10.0, Glib.Gdouble(self.radius + 10.0));
+      Cairo.Line_To(context, 10.0, Glib.Gdouble(self.radius + 10.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius));
+      Cairo.Stroke(context);
+      --
+      -- Draw a horizon line
+      --
+      Cairo.Set_Matrix(context, matrix'Access);
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 1.0, 1.0);
+      Cairo.Move_To(context, Glib.Gdouble(-Float(self.size)), 0.0);
+      Cairo.Line_To(context, Glib.Gdouble(Float(self.size)), 0.0);
+      Cairo.Stroke(context);
+   end;
+   --
+   -- This procedure draws the roll pointer.
+   --
+   procedure draw_roll(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context) is
+   begin
+      Cairo.Rotate(context, Glib.Gdouble((self.roll + 180.0)*Ada.Numerics.Pi/180.0));
+      Cairo.Set_Line_Width(context, 1.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 1.0, 1.0);
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Line_To(context, -10.0, Glib.Gdouble(self.radius - 15.0));
+      Cairo.Line_To(context, 10.0, Glib.Gdouble(self.radius - 15.0));
+      Cairo.Line_To(context, 0.0, Glib.Gdouble(self.radius - 5.0));
+      Cairo.Stroke(context);
+   end;
+   --
+   -- This procedure draws a red X on the widget area.  It is used when data for
+   -- the widget is failed.
+   --
+   procedure draw_failed(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context) is
+   begin
+      Cairo.Set_Line_Width(context, 5.0);
+      Cairo.Set_Source_Rgb(context, 1.0, 0.0, 0.0);
+      Cairo.Move_To(context, 0.0, 0.0);
+      Cairo.Line_To(context, Glib.Gdouble(Float(self.size)), Glib.Gdouble(Float(self.size)));
+      Cairo.Move_To(context, 0.0, Glib.Gdouble(Float(self.size)));
+      Cairo.Line_To(context, Glib.Gdouble(Float(self.size)), 0.0);
+      Cairo.Stroke(context);
+   end;
+
+
+end;
---
+++ bbs-widget-ai.ads
@@ -0,0 +1,93 @@
+with Ada.Text_IO;
+with Ada.Numerics.Generic_Elementary_Functions;
+with Ada.Numerics;
+with Gtkada.Types;
+with Gtk.Main;
+with Gtkada.Builder;
+with Gtk.Widget;
+with Gtk.Misc;
+with Gtk.Level_Bar;
+with Gtk.Progress_Bar;
+with Gtk.Drawing_Area;
+with Gdk.Window;
+with Gdk.Frame_Clock;
+with Glib;
+use type Glib.Gdouble;
+use type Glib.Gint;
+with Glib.Object;
+with Glib.Properties;
+with Cairo;
+package bbs.widget.ai is
+
+   author : constant String := "Brent Seidel";
+   version : constant String := "V00.01";
+   date : constant String := "2-Feb-2016";
+
+   --
+   -- Define a widget for a attitude indicator.  This is an instrument for
+   -- showing the pitch and roll of an aircraft.
+   --
+   type bbs_ai_record is new Gtk.Drawing_Area.Gtk_Drawing_Area_Record with private;
+   type bbs_ai is access all bbs_ai_record'class;
+   --
+   -- Generics used
+   --
+   package math is new Ada.Numerics.Generic_Elementary_Functions(Float);
+   --
+   -- Initialization and setup
+   --
+   function  Get_Type return Glib.GType;
+   procedure gtk_new(self : in out bbs_ai);
+   procedure initialize(self : not null access bbs_ai_record'class);
+   procedure setup(self : not null access bbs_ai_record'class;
+                   radius : Float; parent : Gdk.Gdk_Window);
+   --
+   -- Options
+   --
+   -- This simply sets the value for the ai to display.  It should be within
+   -- the range that the ai can display.
+   --
+   procedure set_value(self : in out bbs_ai_record'Class; pitch : Float; roll : Float);
+   --
+   -- This is called when the data source for the ai has failed or is
+   -- unavailable.  It replaces the ai display with a red "X".
+   --
+   procedure set_failed(self : in out bbs_ai_record'Class; value : Boolean);
+   --
+   -- This controls the slew option of the ai.  If set to false, the ai
+   -- jumps to the new value.  If set to true, the ai moves to the new value
+   -- at a speed set by the rate parameter.  The larger the rate parameter, the
+   -- faster the pointer moves.  The actual speed depends on the rate of the
+   -- callbacks.
+   --
+   procedure set_slew(self : in out bbs_ai_record'Class; value : Boolean; rate : Float);
+
+private
+   type bbs_ai_record is new Gtk.Drawing_Area.Gtk_Drawing_Area_Record with
+      record
+         radius : Float;
+         size : Integer;
+         pitch : Float;
+         roll : Float;
+         slew_rate : Float;
+         dsp_pitch : Float;
+         dsp_roll : Float;
+         failed : Boolean;
+         slew : Boolean;
+         callback_id : Glib.Guint;
+      end record;
+
+   two_pi : constant Float := 2.0*Ada.Numerics.Pi;
+   sqrt_2 : constant Float := math.Sqrt(2.0);
+
+   klass : aliased Glib.Object.Ada_GObject_Class := Glib.Object.Uninitialized_Class;
+
+   function slew_handler(Self : not null access Gtk.Widget.Gtk_Widget_Record'Class;
+                         Frame_Clock : not null access Gdk.Frame_Clock.Gdk_Frame_Clock_Record'Class) return Boolean;
+   function draw_ai(Self : access Gtk.Widget.Gtk_Widget_Record'Class; context : Cairo.Cairo_Context) return boolean;
+   procedure draw_horizon(self : access bbs_ai_record'Class; context : Cairo.Cairo_Context);
+   procedure draw_failed(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context);
+   procedure draw_fixed(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context);
+   procedure draw_roll(Self : access bbs_ai_record'Class; context : Cairo.Cairo_Context);
+
+end;
